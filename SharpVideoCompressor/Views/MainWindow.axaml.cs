486e898e11af9d811213a92a4e427c717af5034f
Luca <dima91@live.it>
2025-10-28 19:18:19 +0100

    Add .net slution and project skeleton

---
+++ SharpVideoCompressor/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SharpVideoCompressor.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
