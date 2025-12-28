e4978bf0f357866b382dc5518159f3721c4cb84e
Luca <dima91@live.it>
2025-12-28 23:29:53 +0100

    Add MainWindow layout with conversion, settings, and job list cards

--- SharpVideoCompressor/ViewModels/MainWindowViewModel.cs
+++ SharpVideoCompressor/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,24 @@
 ﻿namespace SharpVideoCompressor.ViewModels;
 
+using SharpVideoCompressor.ViewModels.Cards;
+
 public class MainWindowViewModel : ViewModelBase
 {
+    
+    private CurrentConversionCardViewModel _CurrentConversionCardVM;
+    public CurrentConversionCardViewModel CurrentConversionCardVM { get => _CurrentConversionCardVM; }
+
+    private GlobalSettingsCardViewModel _GlobalSettingsCardVM;
+    public GlobalSettingsCardViewModel GlobalSettingsCardVM {get => _GlobalSettingsCardVM; }
+
+    private JobListCardViewModel _JobListCardVM;
+    public JobListCardViewModel JobListCardVM {get => _JobListCardVM; }
+
+
+    public MainWindowViewModel ()
+    {
+        _CurrentConversionCardVM = new CurrentConversionCardViewModel();
+        _GlobalSettingsCardVM = new GlobalSettingsCardViewModel();
+        _JobListCardVM = new JobListCardViewModel();
+    }
 }
