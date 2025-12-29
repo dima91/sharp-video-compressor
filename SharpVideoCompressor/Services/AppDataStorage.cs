2cadf04659f2622de5074ce55bb59b87e3426fb0
Luca <dima91@live.it>
2025-12-29 22:47:56 +0100

    Integrates AppDataStorage with ReactiveUI
    Inherits AppDataStorage from ReactiveObject to enable property change notifications and facilitate data binding within the UI using reactive patterns.

--- SharpVideoCompressor/Services/AppDataStorage.cs
+++ SharpVideoCompressor/Services/AppDataStorage.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Reactive.Subjects;
+using ReactiveUI;
 using Splat;
 
 namespace SharpVideoCompressor.Services;
 
-public class AppDataStorage
+public class AppDataStorage : ReactiveObject
 {
     public AppDataStorage()
     {
